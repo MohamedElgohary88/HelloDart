f5d47d00f7b66eb66ec3cb7f3cd4de0712e7b2c8
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2024-08-28 11:22:41 +0300

    calling dart basics, control flow and functions

--- bin/hello_dart.dart
+++ bin/hello_dart.dart
@@ -1,14 +1,21 @@
 import 'package:hello_dart/hello_dart.dart' as hello_dart;
+import 'dart_basics.dart';
+import 'functions.dart';
+import 'control_flow.dart';
 
 void main() {
   print('Hello world!');
-  var obj = MyClass();
-  obj.myMethod();
-}
 
-class MyClass {
-  var num = 10;
-  void myMethod() {
-    print(num);
-  }
+  // Calling the function from dart_basics.dart
+  var objBasics = DartBasics();
+  objBasics.practise();
+
+  // Calling the function from functions.dart
+  var objFunctions = Functions();
+  objFunctions.myFunction();
+
+  // Calling the function from control_flow.dart
+  var objControlFlow = ControlFlow();
+  objControlFlow.practise();
+
 }
